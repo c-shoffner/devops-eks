e606839ed7ab1a9cd07cb9ec74e7a3bdc9cf0e3f
Macbook Pro <cody@shoff.me>
2023-03-24 22:37:02 -0500

    workflows

--- get_outputs.py
+++ get_outputs.py
@@ -1,9 +1,10 @@
-import requests, json, os
+import requests, json, os, base64
 
 tf_api_token = os.getenv('TF_API_TOKEN')
+tf_encoded = str(base64.b64encode(bytes(':{tf_api_token}', 'utf-8')), 'utf-8')
 url = "https://app.terraform.io/api/v2/workspaces/ws-qam9e7EbmmEhKF6n/current-state-version?include=outputs"
 headers = {
-  'Authorization': 'Bearer {tf_api_token}'
+  'Authorization': 'Bearer {tf_encoded}'
 }
 
 response = requests.request("GET", url, headers=headers)
