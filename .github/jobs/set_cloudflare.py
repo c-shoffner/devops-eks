aa6598bae56f9df91413b84185edaac5393f2ac2
Macbook Pro <cody@shoff.me>
2023-03-26 20:45:34 -0500

    workflows

---
+++ .github/jobs/set_cloudflare.py
@@ -0,0 +1,43 @@
+import requests, json, os
+from kubernetes import client, config
+
+
+# Use kube package to get hostname of service
+config.load_kube_config()
+v1 = client.CoreV1Api()
+
+services = v1.list_service_for_all_namespaces(watch=False)
+
+for svc in services.items:
+    if svc.status.load_balancer.ingress:
+        hostname = svc.status.load_balancer.ingress[0].hostname
+        print(svc.status.load_balancer.ingress[0].hostname)
+
+# Call env variables required
+zone_id = os.getenv("ZONE_ID")
+dns_record = os.getenv("DNS_RECORD")
+cloudflare_api = os.getenv("CLOUDFLARE_API")
+
+# set hostname as env var
+env_file = os.getenv("GITHUB_ENV")
+with open(env_file, "a") as myfile:
+  myfile.write(f'HOSTNAME={hostname}')
+
+url = f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/{dns_record}?Content-Type=application%2Fjson'
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {cloudflare_api}'
+    }
+body = json.dumps({
+    'content': f'{hostname}',
+    'name': 'liatrio',
+    'proxied': False,
+    'type': 'CNAME'
+})
+# call cloudflare api and set dns
+response = requests.put(url, headers=headers, data=body)
+print(response.text)
+
+
+
+
---
+++ .github/jobs/tf_cloud_outputs.py
@@ -0,0 +1,36 @@
+import requests, json, os, base64
+
+tf_api_token = os.getenv('TF_API_TOKEN')
+url = "https://app.terraform.io/api/v2/workspaces/ws-qam9e7EbmmEhKF6n/current-state-version?include=outputs"
+headers = {
+  'Authorization': f'Bearer {tf_api_token}'
+}
+
+try:
+  response = requests.request("GET", url, headers=headers)
+  response_json = response.json()
+except:
+  print("Cannot call Terraform Cloud API")
+
+try:
+  values = response_json['included']
+except:
+  print("Cannot find values within Terraform Cloud API")
+
+env_file = os.getenv("GITHUB_ENV")
+for x in values:
+    name = x['attributes']['name']
+    if 'cluster_name' in name:
+        cluster_name = x['attributes']['value']
+        print(cluster_name)
+
+for x in values:
+    name = x['attributes']['name']
+    if 'region' in name:
+        cluster_region = x['attributes']['value']
+        print(cluster_region)
+
+
+with open(env_file, "a") as myfile:
+  myfile.write(f'CLUSTER_NAME={cluster_name}\n')
+  myfile.write(f'CLUSTER_REGION={cluster_region}')
