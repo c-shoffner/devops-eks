b31d2fa2b0f381a87152284f5593fdf34b04882b
Cody Shoffner <cody@shoff.me>
2023-04-12 08:37:53 -0500

    Delete aks_cluster.sh

--- aks_cluster.sh
+++
@@ -1,4 +0,0 @@
-cluster_name="liatrio-eks-0XoKiTgg"
-cluster_region="us-west-2"
-# aws eks update-kubeconfig --name $(terraform output -raw cluster_name) --region $(terraform output -raw region)
-aws eks update-kubeconfig --name $cluster_name --region $cluster_region
