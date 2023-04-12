a007d800cc69ef964eae5db3084c42c2c7682104
Macbook Pro <cody@shoff.me>
2023-04-12 08:35:06 -0500

    scrubbing and updates

--- app/app.py
+++ app/app.py
@@ -4,9 +4,9 @@ from flask import Flask, request
 
 
 app = Flask(__name__)
-@app.route('/liatrio', methods=['GET'])
-def liatrio():
-    message = "Automate all the things!"
+@app.route('/', methods=['GET'])
+def hello_world():
+    message = "Hello World"
     timestamp = datetime.now().timestamp()
 
     payload = json.dumps({
@@ -14,11 +14,6 @@ def liatrio():
         "timestamp": f"{timestamp}"
     })
     return payload, 200
-
-
-@app.route('/', methods=['GET'])
-def main():
-    return "Please call /liatrio for the exercise"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050)
